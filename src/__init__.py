9e5bc8e55c02e77fd684fa33ee4c940574edeea3
Jose Barros <josemiguelbarros@live.com>
2020-10-13 05:15:58 -0300

    Changed test data to be normalized as well, did it before deadline but forgot to push, to prove it you can compare the plots with the ones on the informe

--- src/__init__.py
+++ src/__init__.py
@@ -9,20 +9,25 @@ from utils import one_hot_encoding, normalize_data, split_and_shuffle_train_test
 def iris_dataset_classification():
     # Set the seed to make result reproducible
     np.random.seed(50)
+    # Loads iris dataset
     iris = datasets.load_iris()
     train_data, test_data, train_labels, test_labels = split_and_shuffle_train_test(
         iris.data, iris.target
     )
+    # Does one hot encoding and transposes labels for use in network
     encoding, train_labels = one_hot_encoding(train_labels)
     train_labels = train_labels.T
+    # Normalizes data
     train_data = normalize_data(train_data).T
+    # Creates a range in which we will test different hidden layer number of neurons
     hidden_layer_neurons = range(200, 600, 30)
+    # Here we store the precision for each hidden layer composition
     total_precision = []
     for neuron in hidden_layer_neurons:
         network = TwoLayerNetwork(4, neuron, 3)
-        network.train(train_data, train_labels, 20000, 0.0005)
+        network.train(train_data, train_labels, 10000, 0.001)
         network.plot_training_cost()
-        predictions = network.predict(test_data.T)
+        predictions = network.predict(normalize_data(test_data).T)
         cm = ConfusionMatrix(predictions, test_labels)
         cm.matrix_summary()
         total_precision.append(cm.total_precision())
